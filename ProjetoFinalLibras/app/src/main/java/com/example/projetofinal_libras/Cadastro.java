2f3e48e512364a8838779d456f9a22063513fa05
lizandra18 <lizandra27@alu.ufc.br>
2024-09-11 12:24:31 -0300

    telas cadastro e login

--- ProjetoFinalLibras/app/src/main/java/com/example/projetofinal_libras/Cadastro.java
+++ ProjetoFinalLibras/app/src/main/java/com/example/projetofinal_libras/Cadastro.java
@@ -1,24 +1,75 @@
 package com.example.projetofinal_libras;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Cadastro extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText cadastrarEmail, cadastrarSenha;
+    private Button cadastrarButton;
+    private TextView redirecionaLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        auth = FirebaseAuth.getInstance();
+        cadastrarEmail = findViewById(R.id.cadastrar_email);
+        cadastrarSenha = findViewById(R.id.cadastrar_senha);
+        cadastrarButton = findViewById(R.id.cadastrar_button);
+        redirecionaLogin = findViewById(R.id.redireciona_login);
+
+        cadastrarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = cadastrarEmail.getText().toString().trim();
+                String senha = cadastrarSenha.getText().toString().trim();
+
+                if (usuario.isEmpty()){
+                    cadastrarEmail.setError("Informe um email");
+                }
+                if (senha.isEmpty()){
+                    cadastrarSenha.setError("Informe uma senha");
+                }else {
+                    auth.createUserWithEmailAndPassword(usuario, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Cadastro.this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Cadastro.this, Login.class));
+                            } else {
+                                Toast.makeText(Cadastro.this, "Falha ao tentar cadastrar novo usuário", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        redirecionaLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Cadastro.this, Login.class));
+            }
         });
     }
 }
--- ProjetoFinalLibras/app/src/main/java/com/example/projetofinal_libras/Login.java
+++ ProjetoFinalLibras/app/src/main/java/com/example/projetofinal_libras/Login.java
@@ -1,24 +1,83 @@
 package com.example.projetofinal_libras;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginSenha;
+    private TextView redirecionaCadastro;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginSenha = findViewById(R.id.login_senha);
+        loginButton = findViewById(R.id.login_button);
+        redirecionaCadastro = findViewById(R.id.redireciona_cadastro);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString();
+                String senha = loginSenha.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!senha.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, senha)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(Login.this, "Usuário logado com sucesso", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(Login.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this, "Não foi possível efetuar login", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        loginSenha.setError("Informe a senha");
+                    }
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Informe um email");
+                } else {
+                    loginEmail.setError("Insira um email válido");
+                }
+            }
+        });
+
+        redirecionaCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Cadastro.class));
+            }
         });
     }
 }
